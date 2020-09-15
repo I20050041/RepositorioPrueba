bd5b27be639acf6c42aa7a9e5e62eb42ca4f63c8
I20050041 <71293482+I20050041@users.noreply.github.com>
2020-09-14 21:45:54 -0500

    Add files via upload

---
+++ LaPrueba/LaPrueba/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("primer valor: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("segundo valor: ");
+            b = Int32.Parse(Console.ReadLine());
+            c = a + b;
+            Console.Write("el resultado final de la suma es " + c);
+            Console.ReadKey();
+        }
+    }
+}
